ee2efd9872ee9a4c0d24710c3643f648eb89a59f
Rodrigo <rodrigo.marin.perez@outlook.com>
2019-10-25 19:29:43 +0200

    cambios script

--- scripts/scriptRodrigo.sh
+++ scripts/scriptRodrigo.sh
@@ -15,10 +15,20 @@ echo $WD
 cd $WD/data
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
 
-# place here any commands that need to be run before analysing the samples
+#### Análisis de datos
+
 for sid in $(<list of samples>)
 do
    # place here the script with commands to analyse each sample
+#### Control de calidad utilizando el software FastQC
+if [ "$#" -eq 1 ]
+then
+    sampleid=$1
+    echo "Running FastQC..."
+    mkdir -p out/fastqc
+    fastqc -o out/fastqc data/${sampleid}*.fastq.gz
+    echo
+
    # this command should receive the sample ID as the only argument
 done
 # place here any commands that need to run after analysing the samples
