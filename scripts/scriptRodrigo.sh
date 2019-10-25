498fca057655eb67e53bae4c41fd31355659d8e5
Rodrigo <rodrigo.marin.perez@outlook.com>
2019-10-25 19:11:21 +0200

    cambios script

---
+++ scripts/scriptRodrigo.sh
@@ -0,0 +1,14 @@
+#### Preparando el espacio de trabajo
+
+mkdir -p ~/testsim
+cd ~/testsim
+pwd
+mkdir -p data log out rest scripts
+
+#### Guardamos nuestro directorio de trabajo en una variable
+
+export WD=$(pwd)
+echo $WD
+
+
+
