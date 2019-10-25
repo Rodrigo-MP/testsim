0bd605087025fb56582e7a3040a75eff9c76175f
Rodrigo <rodrigo.marin.perez@outlook.com>
2019-10-25 19:17:59 +0200

    cambios script

--- scripts/scriptRodrigo.sh
+++ scripts/scriptRodrigo.sh
@@ -10,5 +10,15 @@ mkdir -p data log out rest scripts
 export WD=$(pwd)
 echo $WD
 
+#### Descargamos el genoma de E.coli
 
+cd $WD/data
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
 
+# place here any commands that need to be run before analysing the samples
+for sid in $(<list of samples>)
+do
+   # place here the script with commands to analyse each sample
+   # this command should receive the sample ID as the only argument
+done
+# place here any commands that need to run after analysing the samples
