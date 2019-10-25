2397731185142a57f833e849bf12924f299909e9
Rodrigo <rodrigo.marin.perez@outlook.com>
2019-10-25 20:34:19 +0200

    Solo el loop extraído

---
+++ scripts/loop.sh
@@ -0,0 +1,34 @@
+#### Loop de Análisis de datos
+
+for sid in $(ls data/*.fastq.gz | cut -d "_" -f1 | sed 's:data/::' | sort | uniq)
+do
+
+#### Control de calidad utilizando el software FastQC
+
+if [ "$#" -eq 1 ]
+then
+    sampleid=$1
+    echo "Running FastQC..."
+    mkdir -p out/fastqc
+    fastqc -o out/fastqc data/${sampleid}*.fastq.gz
+    echo
+####  Eliminando los adaptadores de las muestras
+    echo "Running cutadapt..."
+    mkdir -p log/cutadapt
+    mkdir -p out/cutadapt
+    cutadapt -m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT -o out/cutadapt/${sampleid}_1.trimmed.fastq.gz -p out/cutadapt/${sampleid}_2.trimmed.fastq.gz data/${sampleid}_1.fastq.gz data/${sampleid}_2.fastq.gz > log/cutadapt/${sampleid}.log
+    echo
+#### Alineamiento con STAR
+    echo "Running STAR alignment..."
+    mkdir -p out/star/${sampleid}
+    STAR --runThreadN 4 --genomeDir res/genome/star_index/ --readFilesIn out/cutadapt/${sampleid}_1.trimmed.fastq.gz out/cutadapt/${sampleid}_2.trimmed.fastq.gz --readFilesCommand zcat --outFileNamePrefix out/star/${sampleid}/
+    echo
+else
+    echo "Usage: $0 <sampleid>"
+    exit 1
+fi
+
+   # this command should receive the sample ID as the only argument
+done
+
+echo
